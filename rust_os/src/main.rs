736eb7b914b9e66e7bb39c226555badd05bde1ef
shafferchance <shafferchance@gmail.com>
2022-05-08 15:48:53 -0700

    Projects

---
+++ rust_os/src/main.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use rust_os::println;
+
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    rust_os::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    rust_os::init();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    
+    rust_os::hlt_loop();
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
